642c62eadd086696c25ca13efe2be4c41cca7305
MithicSpirit <rpc01234@gmail.com>
2020-02-14 21:07:13 -0500

    Creates a basic permission hierarchy
    The permission hierarchy will be used to specify which users
    can use which commands based on their roles/discord perms.
    The hierarchy functions as follows:
    
    * 0 = @everyone
    * 1 = STUDENTS
    * 2 = Trusted Perms
    * 3 = MOD
    * 4 = MEGAMOD, admin

---
+++ checkPerms.js
@@ -0,0 +1,32 @@
+/*
+Permission levels:
+0 = @everyone
+1 = STUDENTS
+2 = Trusted Perms
+3 = MOD
+4 = MEGAMOD, admin
+*/
+
+module.exports = getPermLvl();
+
+const hierarchy = require('./roleHierarchy.json');
+
+const getHierarchyLvl = (member) => {
+	const memberRoles = member.roles;
+	let permLvl = 0;
+	for (const role in memberRoles) {
+		if (role.id in hierarchy) {
+			permLvl = Math.max(permLvl, hierarchy[role.id]);
+		}
+	}
+	return permLvl;
+};
+
+const getPermLvl = (member) => {
+	const perms = member.permissions;
+	if (perms.has('ADMINISTRATOR')) {
+		return 4;
+	} else {
+		return getHierarchyLvl(member);
+	}
+};
