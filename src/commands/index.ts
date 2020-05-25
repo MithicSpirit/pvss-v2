366090019b74d9fea946589204aaa01bbf2c836a
MithicSpirit <rpc01234@gmail.com>
2020-05-25 11:04:43 -0400

    Adds stop to commands/index.ts
    Also adds sorting instructions to that file.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,11 @@
 import help from './help';
 import tempmute from './tempmute';
+import stop from './stop';
+
+// Make sure to sort the items in the map by perms first (low to high), then alphabetically
 
 export default new Map([
 	['help', help],
 	['tempmute', tempmute],
+	['stop', stop],
 ]);
