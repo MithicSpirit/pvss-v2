496f9bbc90dd9df86c0222244f4ad0d5bd692b9f
MithicSpirit <rpc01234@gmail.com>
2020-05-25 10:43:21 -0400

    Data overhaul (fixed!)
    Fixes issue where data was imported and written to different places by always
    reading it from project root directory.

--- src/valueManager.ts
+++ src/valueManager.ts
@@ -1,4 +1,3 @@
-import data from './data.json';
 import fs from 'fs';
 import { Snowflake } from 'discord.js';
 
@@ -6,6 +5,26 @@ interface Mute {
 	id: Snowflake;
 	time: number;
 }
+interface Data {
+	tempMutes: Mute[];
+}
+
+let importSuccess = false;
+let data: Data;
+
+fs.readFile('./data.json', 'utf8', (error, content) => {
+	try {
+		if (error) throw error;
+		data = JSON.parse(content);
+	} catch (e) {
+		console.log(e);
+		console.log('\nData was not imported correctly; please restart.');
+		return;
+	}
+	console.log('Data successfully imported!');
+	importSuccess = true;
+	return;
+});
 
 const tempMutes = (type: 'r' | 'w+' | 'w-', value?: Mute): Mute[] => {
 	try {
@@ -22,9 +41,14 @@ const tempMutes = (type: 'r' | 'w+' | 'w-', value?: Mute): Mute[] => {
 };
 
 const backup = (): void => {
-	fs.writeFile('./data.json', JSON.stringify(data), (error) => {
-		if (error) console.log(error);
-	});
+	if (!importSuccess)
+		console.log('\nData was not imported correctly; please restart.');
+	else {
+		fs.writeFile('./data.json', JSON.stringify(data), 'utf8', (error) => {
+			if (error) console.log(error);
+			else console.log('Backed up.');
+		});
+	}
 };
 
 export default { tempMutes, backup };
