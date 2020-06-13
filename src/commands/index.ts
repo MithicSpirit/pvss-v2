a30e7a5d824b333a02e951fe04947a5da583da48
MithicSpirit <rpc01234@gmail.com>
2020-06-13 12:35:30 -0400

    Actually adds the backup command now
    Forgot to put it in commands/index.ts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,11 +1,13 @@
 import help from './help';
 import tempmute from './tempmute';
 import stop from './stop';
+import backup from './backup';
 
 // Make sure to sort the items in the map by perms first (low to high), then alphabetically
 
 export default new Map([
 	['help', help],
 	['tempmute', tempmute],
+	['backup', backup],
 	['stop', stop],
 ]);
