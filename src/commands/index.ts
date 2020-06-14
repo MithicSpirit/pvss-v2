d85a881f84bb9e8175e4cbcb5f59bb7c4647c524
MithicSpirit <rpc01234@gmail.com>
2020-06-14 13:49:01 -0400

    Adds typings to commands/index.ts
    Should improve typechecking

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,12 +2,21 @@ import help from './help';
 import tempmute from './tempmute';
 import stop from './stop';
 import backup from './backup';
+import { Message, Client } from 'discord.js';
 
+interface Command {
+	run: (args: string[], message?: Message, client?: Client) => string;
+	help: string;
+	desc: string;
+	perms: Number;
+}
 // Make sure to sort the items in the map by perms first (low to high), then alphabetically
 
-export default new Map([
+const command: Map<string, Command> = new Map([
 	['help', help],
 	['tempmute', tempmute],
 	['backup', backup],
 	['stop', stop],
 ]);
+
+export default command;
